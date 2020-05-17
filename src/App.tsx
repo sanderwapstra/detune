25587e0c2aa523f606806c6479b48f2f8ecd65a4
Sander Wapstra <sander.wapstra@in10.nl>
2020-05-17 14:45:57 +0200

    redirect url based on environment

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,14 @@ function App() {
 
     const loginWithSpotify = () => {
         const scopes = 'playlist-modify-public';
+        const redirectUrl =
+            process.env.NODE_ENV === 'development'
+                ? 'https://localhost:3000/'
+                : 'https://discovermusic.netlify.app/';
 
         window.location.replace(
             `https://accounts.spotify.com/authorize?client_id=453ef47ef0c24a63a38a91b855d9c9b3&redirect_uri=${encodeURIComponent(
-                window.location.href
+                redirectUrl
             )}&scope=${encodeURIComponent(scopes)}&response_type=token`
         );
     };
