6043e8334445aa6bcca3dbb346a5f07e8f3dc15b
Sander Wapstra <sander.wapstra@in10.nl>
2020-06-01 21:38:54 +0200

    Rotating div on drag

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactGA from 'react-ga';
+import { animated, useSpring } from 'react-spring';
+import { useDrag } from 'react-use-gesture';
+import useMeasure from 'react-use-measure';
 import { v4 as uuidv4 } from 'uuid';
 import Button from '../Button/Button';
 import StyledLogin from './Login.styles';
 
 const Login: React.FC = () => {
+    const [ref, bounds] = useMeasure();
+    const [{ rotate }, set] = useSpring(() => ({
+        rotate: 0,
+    }));
+
+    // Set the drag hook and define component movement based on gesture data
+    const bind = useDrag(({ event }) => {
+        const { height, width } = bounds;
+        const atan =
+            Math.atan2(event.pageY - height / 2, event.pageX - width / 2) +
+            Math.PI / 2;
+        const degrees = (atan * 180) / Math.PI;
+
+        set({ rotate: degrees, immediate: true });
+    });
+
     const loginWithSpotify = () => {
         ReactGA.event({
             category: 'Login',
@@ -30,9 +49,25 @@ const Login: React.FC = () => {
     };
 
     return (
-        <StyledLogin>
-            <Button click={loginWithSpotify}>Login with Spotify</Button>
-        </StyledLogin>
+        <>
+            <animated.div
+                ref={ref}
+                {...bind()}
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    transform: rotate.interpolate(deg => `rotate(${deg}deg)`),
+                    width: '150px',
+                    height: '150px',
+                    backgroundImage: 'linear-gradient(90deg, #222, #444, #222)',
+                    borderRadius: '50%',
+                }}
+            ></animated.div>
+            <StyledLogin>
+                <Button click={loginWithSpotify}>Login with Spotify</Button>
+            </StyledLogin>
+        </>
     );
 };
 
