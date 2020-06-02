a679180d429aa07808728528fd691f08690b3cd2
Sander Wapstra <sander.wapstra@in10.nl>
2020-06-02 22:40:03 +0200

    progress bar and fixed angle position

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import ReactGA from 'react-ga';
 import { animated, useSpring } from 'react-spring';
 import { useDrag } from 'react-use-gesture';
@@ -15,17 +17,41 @@ const Login: React.FC = () => {
 
     // Set the drag hook and define component movement based on gesture data
     const bind = useDrag(({ event }) => {
-        const { height, width } = bounds;
+        /*
+
+	box.left = hypeDocument.getElementProperty(box, 'left');
+	box.width = hypeDocument.getElementProperty(box, 'width');
+	box.top = hypeDocument.getElementProperty(box, 'top');
+	box.height = hypeDocument.getElementProperty(box, 'height');
+	var boxCenter = [box.left + box.width/2, box.top + box.height/2]
+
+	var angle = Math.atan2((e.pageY- boxCenter[1]),(e.pageX- boxCenter[0]))*(180/Math.PI);
+
+	hypeDocument.setElementProperty(box, 'rotateZ', angle)
+
+        */
+
+        const { height, width, left, top } = bounds;
+        console.log('height, width, left, top :>> ', height, width, left, top);
+        const boxCenter = [left + width / 2, top + height / 2];
+
+        const angle =
+            Math.atan2(
+                event.clientY - boxCenter[1],
+                event.clientX - boxCenter[0]
+            ) *
+            (180 / Math.PI);
+
         const atan =
-            Math.atan2(event.pageY - height / 2, event.pageX - width / 2) +
+            Math.atan2(event.clientY - height / 2, event.clientX - width / 2) +
             Math.PI / 2;
         const degrees = (atan * 180) / Math.PI;
 
-        if (degrees > 140 && degrees < 220) {
-            return;
-        }
+        // if (degrees > 140 && degrees < 220) {
+        //     return;
+        // }
 
-        set({ rotate: degrees, immediate: true });
+        set({ rotate: angle + 90, immediate: true });
     });
 
     const loginWithSpotify = () => {
@@ -54,17 +80,38 @@ const Login: React.FC = () => {
 
     return (
         <>
+            <div
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+
+                    width: '224px',
+                    height: '224px',
+                }}
+            >
+                <CircularProgressbar
+                    value={50}
+                    circleRatio={0.75}
+                    styles={buildStyles({
+                        rotation: 1 / 2 + 1 / 8,
+                        strokeLinecap: 'butt',
+                        trailColor: '#404547',
+                        pathColor: '#87FA5F',
+                    })}
+                />
+            </div>
             <animated.button
                 type="button"
                 ref={ref}
                 {...bind()}
                 style={{
                     position: 'fixed',
-                    top: 0,
-                    left: 0,
+                    top: 32,
+                    left: 32,
                     backgroundColor: '#fff',
-                    width: '200px',
-                    height: '200px',
+                    width: '160px',
+                    height: '160px',
                     borderRadius: '50%',
                     transform: rotate.to(deg => `rotate(${deg}deg)`),
                     transformOrigin: 'center',
@@ -79,7 +126,7 @@ const Login: React.FC = () => {
                         left: '50%',
                         transform: `translate(-50%, 0)`,
                         width: '4px',
-                        height: '90px',
+                        height: 'calc(50% - 10px)',
                         borderRadius: '4px',
                         backgroundColor: 'rgba(0,0,0,0.4)',
                     }}
