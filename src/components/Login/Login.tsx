f6b12495fdb3c93d55018ed7928c0fb6263b3047
Sander Wapstra <sander.wapstra@in10.nl>
2021-03-22 21:47:50 +0100

    Move login logic to seperate file

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,41 +1,14 @@
 import React from 'react';
-import ReactGA from 'react-ga';
-import { v4 as uuidv4 } from 'uuid';
+import { loginWithSpotify } from '../../utils/loginWithSpotify';
 import Button from '../Button/Button';
-import StyledLogin from './Login.styles';
 import Tagline from '../Tagline/Tagline';
+import StyledLogin from './Login.styles';
 
-const Login: React.FC = () => {
-    const loginWithSpotify = () => {
-        ReactGA.event({
-            category: 'Login',
-            action: 'User pressed Login with Spotify button',
-        });
-
-        const state = uuidv4();
-        localStorage.setItem('stateKey', state);
-
-        const scopes = 'playlist-modify-public';
-        const redirectUrl =
-            process.env.NODE_ENV === 'development'
-                ? 'https://localhost:3000/'
-                : 'https://detune.it/';
-
-        window.location.replace(
-            `https://accounts.spotify.com/authorize?client_id=453ef47ef0c24a63a38a91b855d9c9b3&redirect_uri=${encodeURIComponent(
-                redirectUrl
-            )}&scope=${encodeURIComponent(
-                scopes
-            )}&state=${state}&response_type=token`
-        );
-    };
-
-    return (
-        <StyledLogin>
-            <Tagline />
-            <Button click={loginWithSpotify}>Login with Spotify</Button>
-        </StyledLogin>
-    );
-};
+const Login: React.FC = () => (
+    <StyledLogin>
+        <Tagline />
+        <Button click={loginWithSpotify}>Login with Spotify</Button>
+    </StyledLogin>
+);
 
 export default Login;
---
+++ src/utils/loginWithSpotify.ts
@@ -0,0 +1,26 @@
+import ReactGA from 'react-ga';
+import { v4 as uuidv4 } from 'uuid';
+
+export const loginWithSpotify = () => {
+    ReactGA.event({
+        category: 'Login',
+        action: 'User pressed Login with Spotify button',
+    });
+
+    const state = uuidv4();
+    localStorage.setItem('stateKey', state);
+
+    const scopes = 'playlist-modify-public';
+    const redirectUrl =
+        process.env.NODE_ENV === 'development'
+            ? 'https://localhost:3000/'
+            : 'https://detune.it/';
+
+    window.location.replace(
+        `https://accounts.spotify.com/authorize?client_id=453ef47ef0c24a63a38a91b855d9c9b3&redirect_uri=${encodeURIComponent(
+            redirectUrl
+        )}&scope=${encodeURIComponent(
+            scopes
+        )}&state=${state}&response_type=token`
+    );
+};
