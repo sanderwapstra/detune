8c3903b99bc3c89c8b0f8d4051f2c840c2e9807c
Sander Wapstra <sander.wapstra@in10.nl>
2024-02-21 16:38:50 +0100

    Different url

--- src/utils/loginWithSpotify.ts
+++ src/utils/loginWithSpotify.ts
@@ -14,7 +14,7 @@ export const loginWithSpotify = () => {
     const redirectUrl =
         process.env.NODE_ENV === 'development'
             ? 'https://localhost:3000/'
-            : 'https://detune.it/';
+            : 'https://detune.netlify.app/';
 
     window.location.replace(
         `https://accounts.spotify.com/authorize?client_id=453ef47ef0c24a63a38a91b855d9c9b3&redirect_uri=${encodeURIComponent(
